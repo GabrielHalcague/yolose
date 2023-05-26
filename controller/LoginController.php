77270eabaee5821b0ec323c5add58811eff8e939
Gabriel Halcague <42984507+GabrielHalcague@users.noreply.github.com>
2023-05-26 01:11:24 -0300

    arranco nav

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -41,14 +41,19 @@ class LoginController
         $usuarioBuscado = $this->userModel->getUsuario($username,$password);
 
         if($errores == 0 && $usuarioBuscado[0]['activo'] == 1 && $username == $usuarioBuscado[0]['nombreUsuario']){
+
             var_dump(Session::getDataSession());
             Session::set('logged',true);
-            Session::set('rol',1);
+            Session::set('rol',$usuarioBuscado[0]["generoId"]);
             Session::set('username',$username);
+            var_dump(Session::getDataSession());
+
             $data['logged']=true;
-            $data['rol']=1;
+            $data['rol']=$usuarioBuscado[0]["generoId"];
             $data['username']=$username;
+
             $this->rendered->render('home',$data);
+
             exit();
         }else{
             $data['error'] = true;
