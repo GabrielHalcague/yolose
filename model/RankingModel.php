ea41d2fe3bcd5b96aa3194a2b6e751aa9af1c1b5
Gabriel Halcague <42984507+GabrielHalcague@users.noreply.github.com>
2023-06-28 19:38:34 -0300

    comit

--- model/RankingModel.php
+++ model/RankingModel.php
@@ -14,7 +14,7 @@ class RankingModel
         $sql = "select * from (
 select distinct (idUs), puntaje from(
 select idUs,count(n_partida) as puntaje from historialpartidas where estado= 1 and tipoPartida = ".$tipo." group by n_partida, idUs order by puntaje desc)
-as sub group by idUs) as sub2 join usuario where  sub2.idUs = usuario.id
+as sub group by idUs) as sub2 join usuario where  sub2.idUs = usuario.id order by puntaje desc
             limit 10";
         return $this->database->query($sql);
     }
